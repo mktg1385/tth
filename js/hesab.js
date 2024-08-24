74742f82186bc5466a192e1b95b78bf46f2233b5
mohammad krmani <137119712+mktg1385@users.noreply.github.com>
2024-08-24 15:37:28 +0330

    Add files via upload

---
+++ js/hesab.js
@@ -0,0 +1,7 @@
+let fname=document.querySelector(".fname-v");
+let lname=document.querySelector(".lname-v");
+let sabt=document.querySelector("butten");
+name.innerHTML=fname+lname;
+sabt.onclick=function(){
+    
+}
---
+++ test/javas.js
@@ -0,0 +1,13 @@
+let h=document.querySelector(".jf");
+let b=document.querySelector("button");
+let a=document.querySelector("a");
+
+b.onclick=function(){
+    h.innerHTML="nnnnnnnnnnnn";
+}
+if(h=="nnnnnnnnnn")
+{
+    a.onclick=function(){
+        a.href="mm.html";
+    }
+}
